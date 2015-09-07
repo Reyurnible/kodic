c3c7f24a4c8ede839f2e22830532e399837ef0e8
Shun Hosaka <reyurnible@gmail.com>
2015-09-07 22:42:08 +0900

    NavigationDrawerを追加

--- app/src/main/kotlin/com/hosshan/android/godecparents/component/activity/MainActivity.kt
+++ app/src/main/kotlin/com/hosshan/android/godecparents/component/activity/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.hosshan.android.godecparents.component.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.NavigationMenuView
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.FrameLayout
 import butterknife.bindView
 import com.hosshan.android.godecparents.R
 import com.hosshan.android.godecparents.component.fragment.TopFragment
@@ -15,7 +13,9 @@ import com.hosshan.android.godecparents.component.fragment.TopFragment
  */
 public class MainActivity : BaseActivity() {
 
-    val toolbar : Toolbar by bindView(R.id.main_toolbar)
+    val drawerLayout: DrawerLayout by bindView(R.id.main_drawerlayout)
+    val navigationMenu: NavigationMenuView by bindView(R.id.main_navigation_menu)
+    val toolbar: Toolbar by bindView(R.id.main_toolbar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
