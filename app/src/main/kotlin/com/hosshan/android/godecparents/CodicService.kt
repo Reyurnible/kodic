ff40f1701d05b95799f9e2d57bd707e791e643af
hosaka-shun <hosaka_shun@cyberagent.co.jp>
2015-09-10 19:18:44 +0900

    Codicと通信用のRetrofitのサービスを追加

---
+++ app/src/main/kotlin/com/hosshan/android/godecparents/CodicService.kt
@@ -0,0 +1,35 @@
+package com.hosshan.android.godecparents
+
+import com.hosshan.android.godecparents.pojo.CedEntry
+import com.hosshan.android.godecparents.pojo.CedTitle
+import com.hosshan.android.godecparents.pojo.Project
+import com.hosshan.android.godecparents.pojo.TranslatedText
+import retrofit.http.GET
+import retrofit.http.POST
+import retrofit.http.Path
+import retrofit.http.Query
+import rx.Observable
+
+/**
+ * Created by a40875 on 2015/09/10.
+ */
+public interface CodicService {
+
+    @GET("/v1/engine/translate.json")
+    fun getTranslate(@Query("text") text: String): Observable<List<TranslatedText>>
+
+    @POST("/v1/engine/translate.json")
+    fun postTranslate(): Observable<List<TranslatedText>>
+
+    @GET("/v1/user_projects.json")
+    fun getProjectList(): Observable<List<Project>>
+
+    @GET("/v1/user_projects/{id}.json")
+    fun getProject(@Path("id") id: Int): Observable<Project>
+
+    @GET("/v1/ced/lookup.json")
+    fun getCedLookup(@Query("query") query: String, @Query("count") number: Int = 10): Observable<List<CedTitle>>
+
+    @GET("/v1/ced/entries/{id}.json")
+    fun getCedEntry(@Path("id") id: Int): Observable<CedEntry>
+}
