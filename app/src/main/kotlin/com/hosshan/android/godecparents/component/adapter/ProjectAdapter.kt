e4f1d6bcd17a24fc9d5ffd2df6ee50da8465f0d1
Shun Hosaka <reyurnible@gmail.com>
2015-09-12 15:39:59 +0900

    継承クラスを変更

--- app/src/main/kotlin/com/hosshan/android/godecparents/component/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/hosshan/android/godecparents/component/adapter/ProjectAdapter.kt
@@ -1,44 +1,35 @@
 package com.hosshan.android.godecparents.component.adapter
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.hosshan.android.godecparents.model.Project
 import com.hosshan.android.godicparents.R
+import java.util.ArrayList
 
 /**
  * Created by shunhosaka on 15/09/12.
  */
-public class ProjectAdapter(objects: List<Project>) : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>() {
-
-    val objects: List<Project>
-
-    init {
-        this.objects = objects;
-    }
-
-    override fun getItemCount(): Int {
-        return objects.size()
-    }
+public class ProjectAdapter(objects: ArrayList<Project>) : ArrayRecyclerAdapter<Project, ProjectAdapter.ProjectViewHolder>(objects) {
 
     override fun onBindViewHolder(holder: ProjectViewHolder?, position: Int) {
-        val item: Project = objects.get(position)
-        holder?.name?.setText(item.name)
-        holder?.description?.setText(item.description)
-        holder?.owner?.setText(item.owner.name)
+        val item: Project? = getItem(position)
+        item?.let {
+            holder?.name?.setText(item!!.name)
+            holder?.description?.setText(item!!.description)
+            holder?.owner?.setText(item!!.owner.name)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProjectViewHolder? {
-        val view: View = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project, parent, false)
+        val view: View = LayoutInflater.from(parent?.getContext()).inflate(R.layout.item_project, parent, false)
         val viewHolder: ProjectViewHolder = ProjectViewHolder(view)
         return viewHolder;
     }
 
-    public class ProjectViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    public class ProjectViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val name: TextView
         val description: TextView
         val owner: TextView
