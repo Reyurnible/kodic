26b27ae559b5b23b8fe754375e2fad397d7bccef
Shun Hosaka <reyurnible@gmail.com>
2015-09-12 15:49:27 +0900

    Project一覧の取得を追加

--- app/src/main/kotlin/com/hosshan/android/godecparents/component/fragment/ProjectListFragment.kt
+++ app/src/main/kotlin/com/hosshan/android/godecparents/component/fragment/ProjectListFragment.kt
@@ -6,9 +6,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cookpad.android.rxt4a.schedulers.AndroidSchedulers
 import com.hosshan.android.godecparents.component.adapter.ProjectAdapter
 import com.hosshan.android.godecparents.model.Project
+import com.hosshan.android.godecparents.store.ProjectStoreAdapter
 import com.hosshan.android.godicparents.R
+import rx.Subscriber
 import java.util.ArrayList
 import kotlin.platform.platformStatic
 
@@ -40,6 +43,28 @@ public class ProjectListFragment : Fragment() {
         val recyclerView: RecyclerView = view?.findViewById(R.id.project_list_recyclerview) as RecyclerView
         recyclerView.setAdapter(adapter)
 
+        getProjectList()
+
         return view
     }
+
+    private fun getProjectList() {
+        ProjectStoreAdapter
+                .getProjectList(getActivity())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Subscriber<List<Project>>() {
+                    override fun onCompleted() {
+
+                    }
+
+                    override fun onError(e: Throwable?) {
+
+                    }
+
+                    override fun onNext(items: List<Project>?) {
+                        adapter.addAll(items)
+                    }
+                })
+    }
+
 }
