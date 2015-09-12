727a8c0455df8016d1fa848632d45cb06e41206e
Shun Hosaka <reyurnible@gmail.com>
2015-09-12 16:44:18 +0900

    RecyclerViewにLayoutManagerを追加

--- app/src/main/kotlin/com/hosshan/android/godecparents/component/fragment/ProjectListFragment.kt
+++ app/src/main/kotlin/com/hosshan/android/godecparents/component/fragment/ProjectListFragment.kt
@@ -2,6 +2,7 @@ package com.hosshan.android.godecparents.component.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -42,6 +43,7 @@ public class ProjectListFragment : BaseFragment() {
         val view: View? = inflater?.inflate(R.layout.fragment_project_list, container, false)
 
         val recyclerView: RecyclerView = view?.findViewById(R.id.project_list_recyclerview) as RecyclerView
+        recyclerView.setLayoutManager(LinearLayoutManager(getActivity()))
         recyclerView.setAdapter(adapter)
 
         getProjectList()
