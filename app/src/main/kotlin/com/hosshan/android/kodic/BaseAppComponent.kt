0555b1214ed7f3e62c1c85c06d651ee8826e48e7
Shun Hosaka <reyurnible@gmail.com>
2015-10-03 01:57:27 +0900

    add Provide Store

--- app/src/main/kotlin/com/hosshan/android/kodic/BaseAppComponent.kt
+++ app/src/main/kotlin/com/hosshan/android/kodic/BaseAppComponent.kt
@@ -1,6 +1,9 @@
 package com.hosshan.android.kodic
 
 import android.app.Application
+import com.hosshan.android.kodic.store.codic.CedStore
+import com.hosshan.android.kodic.store.codic.EngineStore
+import com.hosshan.android.kodic.store.codic.UserStore
 
 /**
  * Created by shunhosaka on 15/10/03.
@@ -9,4 +12,9 @@ public interface  BaseAppComponent {
 
     public fun application(): Application
 
+    public fun cedStore(): CedStore
+
+    public fun engineStore(): EngineStore
+
+    public fun userStore(): UserStore
 }
