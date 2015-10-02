e255ff1934ca168a48e5425b37efbe4b218c35f0
Shun Hosaka <reyurnible@gmail.com>
2015-10-03 00:33:31 +0900

    add BaseAppComponent

---
+++ app/src/main/kotlin/com/hosshan/android/kodic/BaseAppComponent.kt
@@ -0,0 +1,12 @@
+package com.hosshan.android.kodic
+
+import android.app.Application
+
+/**
+ * Created by shunhosaka on 15/10/03.
+ */
+public interface  BaseAppComponent {
+
+    public fun application(): Application
+
+}
