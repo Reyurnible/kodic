0cc198b63d7fffa63c2a08f7b6c6a394b7969932
Shun Hosaka <reyurnible@gmail.com>
2015-10-11 12:24:43 +0900

    fix Realm history

--- app/src/main/kotlin/com/hosshan/android/kodic/data/realm/TranslatedHistory.kt
+++ app/src/main/kotlin/com/hosshan/android/kodic/data/realm/TranslatedHistory.kt
@@ -1,5 +1,7 @@
 package com.hosshan.android.kodic.data.realm
 
+import com.hosshan.android.kodic.model.TranslatedText
+import com.hosshan.android.kodic.model.Word
 import io.realm.RealmObject
 import io.realm.annotations.RealmClass
 import java.util.*
@@ -10,5 +12,26 @@ import java.util.*
 @RealmClass
 public open class TranslatedHistory(
         public open var createdAt: Date = Date(),
-        public open var translatedText: String? = null
-) : RealmObject() {}
+        public open var successful: Boolean = false,
+        public open var text: String = "",
+        public open var translatedText: String = ""
+) : RealmObject() {
+
+}
+
+public inline fun TranslatedHistory.toTranslatedText(): TranslatedText =
+        TranslatedText(
+                this.successful,
+                this.text,
+                this.translatedText,
+                ArrayList<Word>()
+        )
+
+// Realmのオブジェクトが後にできて、できた依存関係なのでこちらに拡張関数として書く
+public inline fun TranslatedText.toTranslatedHistory(): TranslatedHistory =
+        TranslatedHistory(
+                Date(),
+                this.successful,
+                this.text,
+                this.translatedText
+        )
