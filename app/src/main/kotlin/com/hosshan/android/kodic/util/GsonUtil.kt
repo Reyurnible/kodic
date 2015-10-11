561850f1387146c448ff455fe2b6c14f798b2d2d
Shun Hosaka <reyurnible@gmail.com>
2015-10-11 13:05:14 +0900

    add Gson converter

--- app/src/main/kotlin/com/hosshan/android/kodic/util/GsonUtil.kt
+++ app/src/main/kotlin/com/hosshan/android/kodic/util/GsonUtil.kt
@@ -1,9 +1,10 @@
 package com.hosshan.android.kodic.util
 
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import retrofit.converter.GsonConverter
-import kotlin.platform.platformStatic
+import io.realm.RealmObject
 
 /**
  * Created by a40875 on 2015/09/10.
@@ -13,6 +14,15 @@ public class GsonUtil {
         @JvmStatic fun getInstance(): Gson {
             val builder: GsonBuilder = GsonBuilder()
             builder.setDateFormat("EEE, d MMM yyyy HH:mm:ss Z")
+            builder.setExclusionStrategies(object : ExclusionStrategy {
+                override fun shouldSkipClass(clazz: Class<*>?): Boolean {
+                    return clazz?.declaringClass?.equals(RealmObject::class.java) ?: false
+                }
+
+                override fun shouldSkipField(f: FieldAttributes?): Boolean {
+                    return false
+                }
+            })
             return builder.create()
         }
     }
