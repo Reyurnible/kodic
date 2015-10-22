af82ddc394f458f71f69e34f10610c7981155bf3
Shun Hosaka <reyurnible@gmail.com>
2015-10-22 17:36:51 +0900

    fix and remove subscriver

--- app/src/main/kotlin/com/hosshan/android/kodic/store/codic/CodicRequestSubscriber.kt
+++ app/src/main/kotlin/com/hosshan/android/kodic/store/codic/CodicRequestSubscriber.kt
@@ -1,17 +1,19 @@
 package com.hosshan.android.kodic.store.codic
 
 import android.content.Context
+import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import com.hosshan.android.kodic.R
 import com.hosshan.android.kodic.component.fragment.BaseFragment
 import retrofit.RetrofitError
+import rx.Observer
 import rx.Subscriber
 import timber.log.Timber
 
 /**
  * Created by shunhosaka on 2015/10/16.
  */
-public open class CodicRequestSubscriber<T> : Subscriber<T> {
+public open class CodicRequestSubscriber<T> : Observer<T> {
 
     val context: Context
     val fragment: BaseFragment?
@@ -31,8 +33,8 @@ public open class CodicRequestSubscriber<T> : Subscriber<T> {
     }
 
     override fun onError(error: Throwable?) {
-        error ?: return
         Timber.d("onError:" + error.toString())
+        error ?: return
         if (error is RetrofitError) {
             when (error.response.status) {
                 401 -> {
@@ -40,17 +42,27 @@ public open class CodicRequestSubscriber<T> : Subscriber<T> {
                 }
                 else -> {
                     // Show Error
-                    fragment?.let {
-                        Snackbar.make(it.view, R.string.app_request_error, Snackbar.LENGTH_SHORT).show()
-                    }
+                    showMessage(R.string.app_request_error)
                 }
             }
         }
     }
 
     override fun onNext(t: T?) {
-        Timber.d("onNext:" + t.toString())
+        Timber.d("onNext:" + t?.toString())
+    }
 
+    protected fun showMessage(@StringRes message: Int): Snackbar? {
+        return showMessage(context.getString(message))
+    }
+
+    protected fun showMessage(message: String): Snackbar? {
+        fragment?.let {
+            val snackBar = Snackbar.make(it.view, R.string.app_request_error, Snackbar.LENGTH_SHORT)
+            snackBar.show()
+            return snackBar
+        }
+        return null
     }
 
 }
