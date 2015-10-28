8376b2757a34b2f6103e0a0c6c99eee0716e3c8f
Shun Hosaka <reyurnible@gmail.com>
2015-10-29 02:48:17 +0900

    add license

---
+++ app/src/main/kotlin/com/hosshan/android/kodic/data/local/License.kt
@@ -0,0 +1,20 @@
+package com.hosshan.android.kodic.data.local
+
+/**
+ * Created by shunhosaka on 2015/10/29.
+ */
+enum class License(val title: String, val link: String) {
+    Dagger("Dagger", "https://github.com/google/dagger"),
+    Calligraphy("Calligraphy", "https://github.com/chrisjenx/Calligraphy"),
+    FlowingDrawer("FlowingDrawer", "https://github.com/mxn21/FlowingDrawer"),
+    Glide("Glide", "https://github.com/bumptech/glide"),
+    GlideTransformations("GlideTransformations", "https://github.com/wasabeef/glide-transformations"),
+    Timber("timber", "https://github.com/JakeWharton/timber"),
+    Otto("Otto", "https://github.com/square/otto"),
+    RxJava("RxJava", "https://github.com/ReactiveX/RxJava"),
+    RxBinding("RxBinding", "https://github.com/JakeWharton/RxBinding"),
+    Realm("Realm", "https://github.com/realm/realm-java"),
+    Retrofit("Retrofit", "https://github.com/square/retrofit"),
+    Kotlin("Kotlin", "https://github.com/JetBrains/kotlin"),
+    ;
+}
