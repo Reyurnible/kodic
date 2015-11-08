937662252c3b30225c5b7252608fe5f4c27e459a
Shun Hosaka <reyurnible@gmail.com>
2015-11-08 13:57:04 +0900

    fix hawk init

--- app/src/main/kotlin/com/hosshan/android/kodic/KodicApplication.kt
+++ app/src/main/kotlin/com/hosshan/android/kodic/KodicApplication.kt
@@ -1,5 +1,8 @@
 package com.hosshan.android.kodic
 
+import com.orhanobut.hawk.Hawk
+import com.orhanobut.hawk.HawkBuilder
+import com.orhanobut.hawk.LogLevel
 import timber.log.Timber
 
 /**
@@ -10,5 +13,12 @@ public class KodicApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        Hawk.init(this)
+                .setEncryptionMethod(HawkBuilder.EncryptionMethod.MEDIUM)
+                .setStorage(HawkBuilder.newSharedPrefStorage(this))
+                .setPassword("kodic")
+                .setLogLevel(if (BuildConfig.DEBUG) LogLevel.FULL else LogLevel.NONE)
+                .build()
     }
 }
